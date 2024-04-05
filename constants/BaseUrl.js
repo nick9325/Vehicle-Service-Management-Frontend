26b563dc291b2e4862eb3ff9475851579618d109
Nikhil <nikhildmagar@gmail.com>
2024-04-05 16:21:01 +0530

    base url changed

--- constants/BaseUrl.js
+++ constants/BaseUrl.js
@@ -1,1 +1,2 @@
-export const BaseUrl = "https://vehicle-service-management.onrender.com";
+// export const BaseUrl = "https://vehicle-service-management.onrender.com";
+export const BaseUrl = "http://localhost:8080";
