1b7e6dcf073d87f826c09619d5234ff36b2bd229
Nikhil <nikhildmagar@gmail.com>
2024-04-08 20:52:18 +0530

    scheduled and serviced vehicles integrated

--- app/(dashboard)/vehicles/service-completed/page.jsx
+++ app/(dashboard)/vehicles/service-completed/page.jsx
@@ -2,33 +2,138 @@
 
 import { Col, Row, Card, Container, ListGroup, ListGroupItem } from 'react-bootstrap';
 
+
 import VehicleCard from '../../../../components/VehicleCard'
 import VehiclesNav from '../../../../components/VehiclesNav'
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import toast from 'react-hot-toast';
+import { GetCompletedVehicles } from '../../../../constants/VehicleEndpoints';
 
 
-const ServiceCompleted = () => {
+
+
+
+
+
+
+
+const UnderService = () => {
+
+
+  const router = useRouter();
+  const [vehicleData, setVehicleData] = useState();
+  const [ownerData, setOwnerData] = useState();
+
+  // const fetchOwner = async (ownerId) => {
+
+  //   toast.dismiss();
+  //   toast.loading("Fetching owner..");
+
+  //   const token = localStorage.getItem('token');
+  //   console.log(token)
+  //   var myHeaders = new Headers();
+  //   myHeaders.append("Authorization", `Bearer ${token}`);
+
+  //   const requestOptions = {
+  //     method: "GET",
+  //     headers: myHeaders,
+  //   };
+
+
+  //   let response = await fetch(`${GetOwnerById}${ownerId}`, requestOptions);
+
+
+  //   if (response.ok) {
+  //     let res = await response.json();
+  //     setOwnerData(res);
+
+  //     console.log(ownerData);
+  //     toast.dismiss();
+  //     toast.success('Owner fetched successfully!');
+
+  //   } else if (response.status === 403) {
+  //     toast.dismiss();
+  //     toast.error('Please log in to continue');
+  //     router.push('/authentication/sign-in');
+  //     alert('token expired!');
+  //   } else {
+  //     toast.dismiss();
+  //     toast.error('Failed to fetch owner');
+  //   }
+
+  // }
+
+  const fetchVehicles = async () => {
+
+    toast.dismiss();
+    toast.loading("Fetching vehicles..");
+
+    const token = localStorage.getItem('token');
+    console.log(token)
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${token}`);
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+    };
+
+
+
+
+
+    let response = await fetch(`${GetCompletedVehicles}`, requestOptions);
+
+
+    if (response.ok) {
+      let res = await response.json();
+      setVehicleData(res);
+      console.log(vehicleData);
+  
+   
+      toast.dismiss();
+      toast.success('Vehicles fetched successfully!');
+
+    } else if (response.status === 403) {
+      toast.dismiss();
+      toast.error('Please log in to continue');
+      router.push('/authentication/sign-in');
+      alert('token expired!');
+    } else {
+      toast.dismiss();
+      toast.error('Failed to fetch vehicles');
+    }
+
+  }
+
+
+  useEffect(() => {
+    fetchVehicles();
+  }, []);
+
 
 
   return (
     <Container fluid className="p-6">
 
-      <VehiclesNav/>
+      <VehiclesNav />
 
       <div className="py-3 d-flex gap-3 flex-wrap">
-     
+        {vehicleData  &&  vehicleData.map((vehicle) => (
+          <div key={vehicle.id}>
+            
+            <VehicleCard vehicleModal={vehicle.vehicleModal} vehicleNumber={vehicle.vehicleNumber} vehicleDescription={vehicle.vehicleDescription} serviceStatus={'pending'} buttonName={''} />
 
-        <VehicleCard serviceStatus = {'completed'} buttonName={'Generate Invoice'}/>
-        <VehicleCard serviceStatus = {'completed'} buttonName={'Generate Invoice'}/>
-        <VehicleCard serviceStatus = {'completed'} buttonName={'Generate Invoice'}/>
-        <VehicleCard serviceStatus = {'completed'} buttonName={'Generate Invoice'}/>
-        <VehicleCard serviceStatus = {'completed'} buttonName={'Generate Invoice'}/>
-      
-
-
+          </div>
+        ))}
       </div>
+
+
 
     </Container>
   )
 }
 
-export default ServiceCompleted;
+export default UnderService;
--- app/(dashboard)/vehicles/under-service/page.jsx
+++ app/(dashboard)/vehicles/under-service/page.jsx
@@ -2,34 +2,137 @@
 
 import { Col, Row, Card, Container, ListGroup, ListGroupItem } from 'react-bootstrap';
 
+
 import VehicleCard from '../../../../components/VehicleCard'
 import VehiclesNav from '../../../../components/VehiclesNav'
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import toast from 'react-hot-toast';
+
+import { GetScheduleVehicles } from '../../../../constants/VehicleEndpoints';
+
+
+
+
+
 
 
 const UnderService = () => {
 
 
-    return (
-        <Container fluid className="p-6">
+  const router = useRouter();
+  const [vehicleData, setVehicleData] = useState();
+  const [ownerData, setOwnerData] = useState();
 
-            <VehiclesNav />
+  // const fetchOwner = async (ownerId) => {
 
-            <div className="py-3 d-flex gap-3 flex-wrap">
+  //   toast.dismiss();
+  //   toast.loading("Fetching owner..");
+
+  //   const token = localStorage.getItem('token');
+  //   console.log(token)
+  //   var myHeaders = new Headers();
+  //   myHeaders.append("Authorization", `Bearer ${token}`);
+
+  //   const requestOptions = {
+  //     method: "GET",
+  //     headers: myHeaders,
+  //   };
 
 
-            <VehicleCard serviceStatus = {'inProgress'} buttonName={''}/>
-            <VehicleCard serviceStatus = {'inProgress'} buttonName={''}/>
-            <VehicleCard serviceStatus = {'inProgress'} buttonName={''}/>
-            <VehicleCard serviceStatus = {'inProgress'} buttonName={''}/>
-            <VehicleCard serviceStatus = {'inProgress'} buttonName={''}/>
-         
-          
+  //   let response = await fetch(`${GetOwnerById}${ownerId}`, requestOptions);
+
+
+  //   if (response.ok) {
+  //     let res = await response.json();
+  //     setOwnerData(res);
+
+  //     console.log(ownerData);
+  //     toast.dismiss();
+  //     toast.success('Owner fetched successfully!');
+
+  //   } else if (response.status === 403) {
+  //     toast.dismiss();
+  //     toast.error('Please log in to continue');
+  //     router.push('/authentication/sign-in');
+  //     alert('token expired!');
+  //   } else {
+  //     toast.dismiss();
+  //     toast.error('Failed to fetch owner');
+  //   }
+
+  // }
+
+  const fetchVehicles = async () => {
+
+    toast.dismiss();
+    toast.loading("Fetching vehicles..");
+
+    const token = localStorage.getItem('token');
+    console.log(token)
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${token}`);
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+    };
+
+
+
+
+
+    let response = await fetch(`${GetScheduleVehicles}`, requestOptions);
+
+
+    if (response.ok) {
+      let res = await response.json();
+      setVehicleData(res);
+      console.log(vehicleData);
+  
+   
+      toast.dismiss();
+      toast.success('Vehicles fetched successfully!');
+
+    } else if (response.status === 403) {
+      toast.dismiss();
+      toast.error('Please log in to continue');
+      router.push('/authentication/sign-in');
+      alert('token expired!');
+    } else {
+      toast.dismiss();
+      toast.error('Failed to fetch vehicles');
+    }
+
+  }
+
+
+  useEffect(() => {
+    fetchVehicles();
+  }, []);
+
+
+
+  return (
+    <Container fluid className="p-6">
+
+      <VehiclesNav />
+
+      <div className="py-3 d-flex gap-3 flex-wrap">
+        {vehicleData  &&  vehicleData.map((vehicle) => (
+          <div key={vehicle.id}>
             
+            <VehicleCard vehicleModal={vehicle.vehicleModal} vehicleNumber={vehicle.vehicleNumber} vehicleDescription={vehicle.vehicleDescription} serviceStatus={'scheduled'} />
 
-            </div>
+          </div>
+        ))}
+      </div>
 
-        </Container>
-    )
+
+
+    </Container>
+  )
 }
 
 export default UnderService;
