408ffb31e90ef9208f779da66a1ebc3e1ddedad6
Nikhil <nikhildmagar@gmail.com>
2024-04-11 01:19:14 +0530

    service advisor add,get integrated

---
+++ app/(dashboard)/service-advisors/add/page.jsx
@@ -0,0 +1,221 @@
+'use client'
+
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import toast from 'react-hot-toast';
+
+
+const AddServiceAdvisor = () => {
+
+    const router = useRouter();
+
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        contact: '',
+        address: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleCancel = () => {
+
+        router.push('/service-advisors')
+
+        setFormData({
+            firstName: '',
+            lastName: '',
+            email: '',
+            contact: '',
+            address: '',
+            password: '',
+            confirmPassword: '',
+        });
+    };
+
+
+    const addServiceAdvisor = async () => {
+
+
+        toast.loading('Adding service advisor..');
+
+        var myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'application/json');
+
+        var raw = JSON.stringify({
+            "firstName": formData.firstName,
+            "lastName": formData.lastName,
+            "phone": formData.contact,
+            "address": formData.address,            
+            "email": formData.email,
+            "password": formData.password,
+        }); 
+        console.log(raw)
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        let response = await fetch(`https://vehicle-service-management.onrender.com/auth/signup`, requestOptions);
+
+
+        console.log(response)
+
+        if (response.ok) {
+            let res = await response.json();
+            console.log(res);
+
+            setFormData({
+                firstName: '',
+                lastName: '',
+                email: '',
+                contact: '',
+                address: '',
+                password: '',
+                confirmPassword: '',
+            });
+
+            router.push('/service-advisors')
+
+            toast.dismiss();
+            toast.success('Service advisor added successfully!');
+        } else if (response.status === 403) {
+            toast.dismiss();
+            router.push('/authentication/sign-in');
+            toast.error('Please log in to continue');
+        } else {
+            toast.dismiss();
+            toast.error('Failed to add service advisor');
+        }
+    } 
+
+    const handleAddServiceAdvisor = (e) => {
+        e.preventDefault();
+
+        if(formData.firstName===""){
+            toast.dismiss()
+            toast.error("Please enter firstname")
+        }
+        else if(formData.lastName===""){
+            toast.dismiss()
+            toast.error("Please enter lastname")
+        }
+        else if(formData.contact===""){
+            toast.dismiss()
+            toast.error("Please enter phone")
+        }
+        else if(formData.address===""){
+            toast.dismiss()
+            toast.error("Please enter address")
+        }
+        else if(formData.email===""){
+            toast.dismiss()
+            toast.error("Please enter email")
+        }
+        else if(formData.password===""){
+            toast.dismiss();
+            toast.error("Please enter password")
+        }
+        else if(formData.confirmPassword===""){
+            toast.dismiss();
+            toast.error("Please enter confirm password")
+        }
+        else if(formData.password !== formData.confirmPassword){
+            toast.dismiss();
+            toast.error("Passwords mismatched!")
+        }
+        else{
+            addServiceAdvisor();
+        }
+
+    }
+
+
+    return (
+        <Container fluid className="p-6">
+            <div className="py-6">
+                <Row>
+                    <div className='container'>
+
+                        <Row className="justify-content-center">
+                            <Col>
+                                <Card>
+                                    <Card.Header className="bg-primary text-white">
+                                        <Card.Title className="mb-0">Service Advisor Registration</Card.Title>
+                                    </Card.Header>
+                                    <Card.Body>
+                                        <Form>
+                                            <Row >
+                                                <Col md={6} className="mb-3">
+                                                    <Form.Label htmlFor="firstName">First Name</Form.Label>
+                                                    <Form.Control type="text" id="firstName" name="firstName" placeholder="Enter your first name" value={formData.firstName} onChange={handleChange} />
+                                                </Col>
+                                                <Col md={6} className="mb-3">
+                                                    <Form.Label htmlFor="fullName">Last Name</Form.Label>
+                                                    <Form.Control type="text" id="lastName" name="lastName" placeholder="Enter your last name" value={formData.lastName} onChange={handleChange} />
+                                                </Col>
+
+                                            </Row>
+                                            <Row >
+                                                <Col md={6} className="mb-3">
+                                                    <Form.Label htmlFor="contact">Phone</Form.Label>
+                                                    <Form.Control type="text" id="contact" name="contact" placeholder="Enter contact number" value={formData.contact} onChange={handleChange} />
+                                                </Col>
+                                                <Col md={6} className="mb-3">
+                                                    <Form.Label htmlFor="email">Email</Form.Label>
+                                                    <Form.Control type="email" id="email" name="email" placeholder="name@example.com" value={formData.email} onChange={handleChange} />
+                                                </Col>
+
+                                            </Row>
+                                            <Row>
+                                            <Col md={6} className="mb-3">
+                                                    <Form.Label htmlFor="password">Password</Form.Label>
+                                                    <Form.Control type="password" id="password" name="password" placeholder="Enter password" value={formData.password} onChange={handleChange} />
+                                                </Col>
+                                                <Col md={6} className="mb-3">
+                                                    <Form.Label htmlFor="confirm Password">Confirm Password</Form.Label>
+                                                    <Form.Control type="password" id="confirmPassword" name="confirmPassword" placeholder="confirmPassword" value={formData.confirmPassword} onChange={handleChange} />
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col md={12} className="mb-3">
+                                                    <Form.Label htmlFor="address">Address</Form.Label>
+                                                    <Form.Control type="text" id="address" name="address" placeholder="Enter address" value={formData.address} onChange={handleChange} />
+                                                </Col>
+                                            </Row>
+                                            <Button variant="primary" type="submit" onClick={handleAddServiceAdvisor}>Add Service Advisor</Button>
+                                            <Button variant="secondary" className="ms-2" onClick={handleCancel}>Cancel</Button>
+                                        </Form>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+
+
+
+                    </div>
+
+                </Row>
+
+            </div>
+
+        </Container>
+    )
+}
+
+export default AddServiceAdvisor;
--- app/(dashboard)/service-advisors/page.jsx
+++ app/(dashboard)/service-advisors/page.jsx
@@ -1,72 +1,251 @@
 'use client'
 
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { Col, Row, Container, Table, Button, Modal, Card, Form } from 'react-bootstrap';
+import toast from 'react-hot-toast';
+import Spinner from 'react-bootstrap/Spinner';
 
-import { Col, Row, Container, Table, Button, ButtonGroup } from 'react-bootstrap';
-import { PageHeading, PageHeadingWithButton } from 'widgets'
 
 
 const ServiceAdvisors = () => {
+    const router = useRouter();
+    const [show, setShow] = useState(false);
+    const [serviceAdvisorsData, setServiceAdvisorsData] = useState([]);
+    const [selectedserviceAdvisor, setSelectedserviceAdvisor] = useState(null);
+    const [deletingserviceAdvisor, setDeletingserviceAdvisor] = useState(null);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [loading, setLoading] = useState(true)
+
+
+    const handleClose = () => setShow(false);
+    const handleShow = (serviceAdvisor) => {
+        setSelectedserviceAdvisor(serviceAdvisor);
+        setShow(true);
+    };
+
+    const handleDelete = async () => {
+        setShow(false);
+        if (deletingserviceAdvisor) {
+            toast.loading('Deleting service advisor..')
+            const token = localStorage.getItem('token');
+            const myHeaders = new Headers();
+            myHeaders.append('Authorization', `Bearer ${token}`);
+            const requestOptions = {
+                method: 'DELETE',
+                headers: myHeaders,
+            };
+
+            try {
+                const response = await fetch(`https://vehicle-service-management.onrender.com/user/delete?id=${deletingserviceAdvisor.id}`, requestOptions);
+                if (response.ok) {
+                    toast.dismiss();
+                    toast.success('Service advisor deleted successfully!');
+                    fetchServiceAdvisors();
+
+
+                } else {
+                    toast.dismiss();
+                    toast.error('Failed to delete service advisor');
+                }
+            } catch (error) {
+                console.error('Error occurred while deleting service advisor:', error);
+                toast.error('An error occurred while deleting service advisor');
+            }
+        }
+    };
+
+
+
+    const fetchServiceAdvisors = async () => {
+
+
+        const token = localStorage.getItem('token');
+        const myHeaders = new Headers();
+        myHeaders.append('Authorization', `Bearer ${token}`);
+
+
+        const requestOptions = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "follow"
+        };
+
+        try {
+            const response = await fetch(`https://vehicle-service-management.onrender.com/user/get/all/serviceAdvisor`, requestOptions);
+            if (response.ok) {
+                const serviceAdvisors = await response.json();
+                setServiceAdvisorsData(serviceAdvisors);
+                setLoading(false);
+            } else if (response.status === 403) {
+                toast.dismiss();
+                toast.error('Please log in to continue');
+                router.push('/authentication/sign-in');
+            } else {
+                toast.dismiss();
+                toast.error('Failed to fetch service advisors');
+            }
+        } catch (error) {
+            console.error('Error occurred while fetching service advisors:', error);
+            toast.dismiss();
+            toast.error('An error occurred while fetching service advisors');
+        }
+    };
+
+    useEffect(() => {
+        fetchServiceAdvisors();
+    }, []);
+
+    const filteredserviceAdvisors = serviceAdvisorsData.filter(
+        (serviceAdvisor) =>
+            serviceAdvisor.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            serviceAdvisor.lastName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            serviceAdvisor.email.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const pageSize = 5;
+    const totalItems = filteredserviceAdvisors.length;
+    const totalPages = Math.ceil(totalItems / pageSize);
+
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+    const currentItems = filteredserviceAdvisors.slice(startIndex, endIndex + 1);
+
+    const handleNextPage = () => {
+        setCurrentPage((prevPage) => prevPage + 1);
+    };
+
+    const handlePrevPage = () => {
+        setCurrentPage((prevPage) => prevPage - 1);
+    };
+
     return (
         <Container fluid className="p-6">
 
-            <PageHeadingWithButton heading="Service Advisors" buttonName="Add New Service Advisor" />
-
-
-
-
+            <Row>
+                <Col lg={12} md={12} xs={12}>
+                    <div className="border-bottom pb-4 mb-4 d-flex justify-content-between">
+                        <h3 className="mb-0 fw-bold">Service Advisors</h3>
+                        <Button onClick={() => router.push('/service-advisors/add')} variant="primary" className="">
+                            Add New Service Advisor
+                        </Button>
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col lg={12} md={12} xs={12}>
+                    <Form.Group controlId="formBasicSearch">
+                        <Form.Control
+                            type="text"
+                            placeholder="Search by name or email"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                    </Form.Group>
+                </Col>
+            </Row>
             <div className="py-6">
                 <Row>
+                    <div className={`container ${!loading?'overflow-x-auto':''} `}>
 
-                    <Table className="text-nowrap">
-                        <thead >
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Email</th>
-                                <th scope="col"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Prasad Hiwarkar</td>
-                                <td>prasad@gmail.com</td>
-                                <td>
-                                    <Button variant="success" size='sm' className="me-2"><i className="fe fe-edit"></i></Button>
- 
-                                    <Button variant="danger" size='sm' className=""><i className="fe fe-trash-2"></i></Button>
-                                </td>
+                        {!loading ?
 
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Vaibhavi Hule</td>
-                                <td>vaibhavi@gmail.com</td>
-                                <td>
-                                    <Button variant="success" size='sm' className="me-2"><i className="fe fe-edit"></i></Button>
- 
-                                    <Button variant="danger" size='sm' className=""><i className="fe fe-trash-2"></i></Button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Nikhil Magar</td>
-                                <td>nikhil@gmail.com</td>
-                                <td>
-                                    <Button variant="success" size='sm' className="me-2"><i className="fe fe-edit"></i></Button>
- 
-                                    <Button variant="danger" size='sm' className=""><i className="fe fe-trash-2"></i></Button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </Table>
+                            <Table className="text-nowrap">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
 
+                                    {currentItems.map((serviceAdvisor, index) => (
+                                        <tr key={startIndex + index + 1}>
+                                            <Modal show={show && selectedserviceAdvisor === serviceAdvisor} onHide={handleClose}>
+                                                <Modal.Header closeButton>
+                                                    <Modal.Title>Confirm Deletion</Modal.Title>
+                                                </Modal.Header>
+                                                <Modal.Body>
+                                                    Are you sure you want to delete {serviceAdvisor.firstName} {serviceAdvisor.lastName}?
+                                                </Modal.Body>
+                                                <Modal.Footer>
+                                                    <Button variant="secondary" onClick={handleClose}>
+                                                        Cancel
+                                                    </Button>
+                                                    <Button variant="danger" onClick={handleDelete}>
+                                                        Delete
+                                                    </Button>
+                                                </Modal.Footer>
+                                            </Modal>
+                                            <th scope="row">{startIndex + index + 1}</th>
+                                            <td>
+                                                {serviceAdvisor.firstName} {serviceAdvisor.lastName}
+                                            </td>
+                                            <td>{serviceAdvisor.email}</td>
+                                            <td>
+                                                <Button
+                                                    onClick={() =>
+                                                        router.push(
+                                                            `/service-advisors/view?firstName=${serviceAdvisor.firstName}&lastName=${serviceAdvisor.lastName}&email=${serviceAdvisor.email}&phone=${serviceAdvisor.phone}&address=${serviceAdvisor.address}`
+                                                        )
+                                                    }
+                                                    variant="secondary"
+                                                    size="sm"
+                                                    className="me-2"
+                                                >
+                                                    <i className="fe fe-eye"></i>
+                                                </Button>
+                                                <Button
+                                                    onClick={() =>
+                                                        router.push(
+                                                            `/service-advisors/edit?id=${serviceAdvisor.id}&firstName=${serviceAdvisor.firstName}&lastName=${serviceAdvisor.lastName}&email=${serviceAdvisor.email}&phone=${serviceAdvisor.phone}&address=${serviceAdvisor.address}`
+                                                        )
+                                                    }
+                                                    variant="success"
+                                                    size="sm"
+                                                    className="me-2"
+                                                >
+                                                    <i className="fe fe-edit"></i>
+                                                </Button>
+                                                <Button
+                                                    onClick={() => {
+                                                        handleShow(serviceAdvisor);
+                                                        setDeletingserviceAdvisor(serviceAdvisor);
+                                                    }}
+                                                    variant="danger"
+                                                    size="sm"
+                                                    className=""
+                                                >
+                                                    <i className="fe fe-trash-2"></i>
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table> : <div className='text-center'><Spinner animation="border" />
+                            </div>}
+                    </div>
                 </Row>
-
+                {totalPages > 1 && (
+                    <Row className="mt-4">
+                        <Col className="d-flex justify-content-center">
+                            {currentPage > 1 && (
+                                <Button onClick={handlePrevPage} className="me-2">
+                                    Prev
+                                </Button>
+                            )}
+                            {currentPage < totalPages && (
+                                <Button onClick={handleNextPage}>Next</Button>
+                            )}
+                        </Col>
+                    </Row>
+                )}
             </div>
-
         </Container>
-    )
-}
+    );
+};
 
 export default ServiceAdvisors;
