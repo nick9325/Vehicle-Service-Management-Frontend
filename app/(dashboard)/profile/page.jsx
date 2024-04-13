3740619785ec5eea59ce2ca93980711e7333a483
Nikhil <nikhildmagar@gmail.com>
2024-04-14 01:42:09 +0530

    profile page added

--- app/(dashboard)/profile/page.jsx
+++ app/(dashboard)/profile/page.jsx
@@ -1,34 +1,43 @@
 'use client'
 
-import { Col, Row, Container } from 'react-bootstrap';
+import { Row, Container } from 'react-bootstrap';
+import ProfileHeader from '../../../components/ProfileHeader';
+import Spinner from 'react-bootstrap/Spinner';
 
 
-
-import {
-  AboutMe,
-  ProfileHeader,
-  ProjectsContributions,
-
-} from 'sub-components'
+import { useGlobalContext } from '../context/globalContext';
+import AboutUser from '../../../components/AboutUser';
 
 const Profile = () => {
+
+
+  const { user } = useGlobalContext();
+
+
   return (
     <Container fluid className="p-6">
 
-      <ProfileHeader />
+      {user?<div>
+
+      <ProfileHeader firstName={user.firstName} lastName={user.lastName} />
 
 
       <div className="py-6">
         <Row>
 
 
-          <AboutMe />
+          <AboutUser firstName={user.firstName} lastName={user.lastName} email={user.email} phone={user.phone} address={user.address} />
 
-          <ProjectsContributions />
+  
 
 
         </Row>
       </div>
+
+
+      </div>:<div className='text-center'>
+            <Spinner animation="border" />
+          </div>}
 
     </Container>
   )
