4122c82f0f81f1cd91469a1b65cb52234720cdeb
Nikhil <nikhildmagar@gmail.com>
2024-04-25 01:42:09 +0530

    final clean up

--- widgets/index.js
+++ widgets/index.js
@@ -1,20 +1,11 @@
-// import widget/custom components from highlight-code folder 
-import HighlightCode from 'widgets/highlight-code/HighlightCode';
+
 import PageHeading from 'widgets/PageHeading';
 import PageHeadingWithButton from './PageHeadingWithButton';
-import FormSelect from 'widgets/form-select/FormSelect';
-import PricingCard from 'widgets/cards/PricingCard';
-import FeatureLeftTopIcon from 'widgets/features/FeatureLeftTopIcon';
-import { DropFiles } from 'widgets/dropfiles/DropFiles';
-import StatRightTopIcon from 'widgets/stats/StatRightTopIcon';
+
 
 export {
-   HighlightCode,
+
    PageHeadingWithButton,
    PageHeading,
-   FormSelect,
-   PricingCard,
-   FeatureLeftTopIcon,
-   DropFiles,
-   StatRightTopIcon
+  
 };
